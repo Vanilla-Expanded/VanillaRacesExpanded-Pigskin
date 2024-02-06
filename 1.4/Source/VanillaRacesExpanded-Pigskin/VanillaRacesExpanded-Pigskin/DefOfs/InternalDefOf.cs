c35e7dbedda7e211202717053f77202958a639a4
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-02-06 13:43:41 +0100

    v0.1

---
+++ 1.4/Source/VanillaRacesExpanded-Pigskin/VanillaRacesExpanded-Pigskin/DefOfs/InternalDefOf.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace VanillaRacesExpandedPigskin
+{
+    [DefOf]
+    public static class InternalDefOf
+    {
+        static InternalDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(InternalDefOf));
+        }
+
+        public static GeneDef VRE_PorkFlesh;
+
+        public static ThingDef Meat_Pig;
+
+        [MayRequireIdeology]
+        public static PreceptDef Cannibalism_Acceptable;
+        [MayRequireIdeology]
+        public static PreceptDef Cannibalism_Preferred;
+        [MayRequireIdeology]
+        public static PreceptDef Cannibalism_RequiredStrong;
+        [MayRequireIdeology]
+        public static PreceptDef Cannibalism_RequiredRavenous;
+
+    }
+}
