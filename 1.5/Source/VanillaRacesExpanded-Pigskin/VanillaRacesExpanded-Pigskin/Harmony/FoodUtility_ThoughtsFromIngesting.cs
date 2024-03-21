9c585e619d14169493f30a7f5dac5c07a9a339f8
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-03-21 12:34:45 +0100

    Updated to 1.5

---
+++ 1.5/Source/VanillaRacesExpanded-Pigskin/VanillaRacesExpanded-Pigskin/Harmony/FoodUtility_ThoughtsFromIngesting.cs
@@ -0,0 +1,59 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+using System.Collections.Generic;
+
+namespace VanillaRacesExpandedPigskin
+{
+    [HarmonyPatch(typeof(FoodUtility))]
+    [HarmonyPatch("ThoughtsFromIngesting")]
+    public static class VanillaRacesExpandedPigskin_FoodUtility_ThoughtsFromIngesting_Patch
+    {
+
+        public static List<PreceptDef> listOfPrecepts = new List<PreceptDef>(){InternalDefOf.Cannibalism_Acceptable, InternalDefOf.Cannibalism_Preferred,
+                InternalDefOf.Cannibalism_RequiredStrong, InternalDefOf.Cannibalism_RequiredRavenous };
+
+        [HarmonyPostfix]
+        public static void AddPorkThought(Pawn ingester, Thing foodSource, ThingDef foodDef)
+        {
+            if (ingester.Ideo != null && ingester.genes?.HasGene(InternalDefOf.VRE_PorkFlesh) == true)
+            {
+                if (foodDef== InternalDefOf.Meat_Pig && IdeoNotCannibal(ingester))
+                {
+                    Thought_Memory thought_Memory = ThoughtMaker.MakeThought(InternalDefOf.AteHumanlikeMeatDirectCannibal, null);
+                    ingester.needs.mood.thoughts.memories.TryGainMemory(thought_Memory);
+
+                    ingester.mindState.lastHumanMeatIngestedTick = Find.TickManager.TicksGame;
+                    Find.HistoryEventsManager.RecordEvent(new HistoryEvent(HistoryEventDefOf.AteHumanMeat, ingester.Named(HistoryEventArgsNames.Doer)), canApplySelfTookThoughts: false);
+                }
+            }
+        }
+
+        public static bool IdeoNotCannibal(Pawn pawn)
+        {
+            foreach (PreceptDef precept in listOfPrecepts)
+            {
+                if (pawn.Ideo.HasPrecept(precept))
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
